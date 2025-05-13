6dc8499df081b3f3a14147d773b78d0f6b622c3a
Jerome Duncan <hello@itsjero.me>
2025-05-13 17:33:50 +0100

    feat: bump phpvm to 1.1.0

--- Formula/phpvm.rb
+++ Formula/phpvm.rb
@@ -3,8 +3,8 @@ class Phpvm < Formula
   homepage "https://github.com/jrmd/phpvm" # Replace with your actual homepage
 
   # Define the URL to your source code archive and its SHA-256 checksum
-  url "https://github.com/jrmd/phpvm/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "ed071fb074f8ca7bb4c0311d1bcb0c7a56a94e6102f58bac3cfd588f0135eea3"
+  url "https://github.com/jrmd/phpvm/archive/refs/tags/1.1.0.tar.gz"
+  sha256 "e3bedc6339de13db9035ac15134bd0b467152da79281e98686e4108969111641"
   depends_on "go" => :build # Specify that Go is needed for building
 
   def install
